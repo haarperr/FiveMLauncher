de956eee825632200f3fb2c0a6d95432cdd88b8d
Rzy <45715088+Riziebtw@users.noreply.github.com>
2020-03-26 17:38:11 +0100

    yeet

---
+++ FiveMLauncherTemplateBYRZY/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FiveMLauncherTemplateBYRZY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.animaForm1 = new Plot.Anima.Anima.AnimaForm();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.connectbtn = new Plot.Anima.Anima.AnimaButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.animaForm1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // animaForm1
+            // 
+            this.animaForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.animaForm1.Controls.Add(this.pictureBox3);
+            this.animaForm1.Controls.Add(this.pictureBox2);
+            this.animaForm1.Controls.Add(this.connectbtn);
+            this.animaForm1.Controls.Add(this.pictureBox1);
+            this.animaForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.animaForm1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.animaForm1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.animaForm1.Location = new System.Drawing.Point(0, 0);
+            this.animaForm1.Name = "animaForm1";
+            this.animaForm1.Size = new System.Drawing.Size(800, 450);
+            this.animaForm1.TabIndex = 0;
+            this.animaForm1.Text = "FiveM Server Launcher Template | Mady By RZY#2004";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 412);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // connectbtn
+            // 
+            this.connectbtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.connectbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.connectbtn.ImageLocation = new System.Drawing.Point(30, 6);
+            this.connectbtn.ImageSize = new System.Drawing.Size(14, 14);
+            this.connectbtn.Location = new System.Drawing.Point(554, 361);
+            this.connectbtn.Name = "connectbtn";
+            this.connectbtn.Size = new System.Drawing.Size(234, 77);
+            this.connectbtn.TabIndex = 1;
+            this.connectbtn.Text = "Se connecter";
+            this.connectbtn.UseVisualStyleBackColor = true;
+            this.connectbtn.Click += new System.EventHandler(this.connectbtn_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 377);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(113, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(122, 377);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(113, 70);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.animaForm1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.animaForm1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Plot.Anima.Anima.AnimaForm animaForm1;
+        private Plot.Anima.Anima.AnimaButton connectbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ FiveMLauncherTemplateBYRZY/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiveMLauncherTemplateBYRZY
+{
+    public partial class Form1 : Form
+    {
+
+        /* Voila, pas plus compliqué. On 3 process.start et un petit design de fils de pute et certains font payer ça :^)
+        Arretez d'éssayer de vendre des choses comme ça, ça va rien vous apporter et ne vous sentez pas dev en faisant ça, svp.
+        Bon ça faisait un bon moment j'avais pas lancé VS et j'ai un un peu de mal en sah j'ai tous les automatisme du lua maintenant, et plus du C# rip..
+        Faites vous plaisir, dédi au poto plot pour le framework du design.
+        PS: Inutile de vendre, ça sert à rien.
+             */ 
+        public Form1()
+        {
+            InitializeComponent();
+            // on rend les deux picture box transparente et ayant pour fond la picturebox1
+            pictureBox1.Controls.Add(pictureBox2);
+            pictureBox1.Controls.Add(pictureBox3);
+
+            pictureBox2.Location = new Point(5, 337);
+            pictureBox2.BackColor = Color.Transparent;
+
+            pictureBox3.Location = new Point(113, 337);
+            pictureBox3.BackColor = Color.Transparent;
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://twitter.com");
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://top-serveurs.net/");
+        }
+
+        private void connectbtn_Click(object sender, EventArgs e)
+        {
+            Process.Start("Explorer.exe", "fivem://connect/51.38.215.133:27035");
+        }
+    }
+}
